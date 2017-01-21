96c58092a0551ab3b1ec52db59239b395524954f
Sudheesh Bhattarai <sudheeshbhattarai@Sudheeshs-MacBook-Pro.local>
2017-01-21 15:52:02 -0500

    Reversing a list works. Need to reverse list inside of list.

--- fp.rkt
+++ fp.rkt
@@ -3,7 +3,7 @@
 (define (reverse-general L)
   (cond
      ((Null? l) l)
-     (else (append (reverse-general (cdr l)) (list car L)))))
+     (else (append (reverse-general (cdr l)) (list (car L))))))
    
        
 (reverse-general '(a b (c (d e)) f))
