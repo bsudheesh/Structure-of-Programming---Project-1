430c183b998f6dbaea8fe256c78a2b2a1f568b52
Sudheesh Bhattarai <sudheeshbhattarai@Sudheeshs-MacBook-Pro.local>
2017-01-22 09:10:59 -0500

    Question 2: Checking to see if list, number or symbol and calling function. Wrong output.

--- fp.rkt
+++ fp.rkt
@@ -3,10 +3,11 @@
 ;We check to see if the list is NULL, if yes, we return the list. This is the return statement in the recursive call.
 ;Next we check if the element is the list, if yes, we recursively call the function for the first element, and for the remaining element
 ;If the element is an atom, we append the first element and recursively call the remaining list with the first element popped.
+
 (define (reverse-general L)
   (cond
      ((Null? l) l) ;checking if the list is NULL
-     ((list? (car l)) (append (reverse-general (cdr l)) (list (reverse-general (car l))))) ;if it's list, we append the first element and call the function with the first element popped
+     ((list? (car l)) (append (reverse-general (cdr l)) (list (reverse-general (car l))))) ;if it's list, we append the first element by making a recursive call for the atom and call the function with the first element popped
      (else (append (reverse-general (cdr l)) (list (car L)))))) ;if an atom, we append the first element and call the function with the first element popped
 
 
@@ -14,4 +15,21 @@
    
        
 (reverse-general '(a b c))
+
+;Question 2
+; Write a function (sum-up-numbers-general L). L is a list, which may contain
+;as elements numbers and non-numbers. The result of the function is the sum of all the numbers
+;(including those in nested lists) in L. If there are no such numbers, the result is zero. For
+;example, the result of (sum-up-numbers-general ‘(a b 1 (2 c (3)) d))
+;should be 6.
+
+(define  (sum-up-numbers-general L)
+  (cond
+    ((NULL? L) 0)
+    (list? sum-up-numbers-general (list (cdr L)))
+    (number? (car L) (+ (car L) (recursive-sum (cdr L))))
+    (else (sum-up-numbers-general (list (cdr L))))))
+
+(sum-up-numbers-general '(1 2 3))
+  
   
