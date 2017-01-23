b6a12922e31c2a6401d1136d125a5c9113e856c2
Sudheesh Bhattarai <sudheeshbhattarai@Sudheeshs-MacBook-Pro.local>
2017-01-23 17:08:21 -0500

    Checked test cases and added comments.

--- fp.rkt
+++ fp.rkt
@@ -1,8 +1,9 @@
 ;Question 1
+;Logic
 ;The idea here is to append an atom / list recursively till we reach the very end of the element given to us
-;We check to see if the list is NULL, if yes, we return the list. This is the return statement in the recursive call.
-;Next we check if the element is the list, if yes, we recursively call the function for the first element, and for the remaining element
-;If the element is an atom, we append the first element and recursively call the remaining list with the first element popped.
+;1) We check to see if the list is NULL, if yes, we return the list. This is the return statement in the recursive call.
+;2) Next we check if the element is the list, if yes, we recursively call the function for the first element, and for the remaining element
+;3) If the element is an atom, we append the first element and recursively call the remaining list with the first element popped.
 
 (define (reverse-general L)
   (cond
@@ -15,10 +16,11 @@
 
 
 ;Question 2
+;Logic
 ;The idead is to check if the first element of the list is an interger.
-;If null, we return 0
-;If it is an Interger, we perform an addition of the first element and recursively call the function by popping the first element.
-;Else we recursively call the function by popping the first element of the list
+;1) If null, we return 0
+;2) If it is an Interger, we perform an addition of the first element and recursively call the function by popping the first element.
+;3) Else we recursively call the function by popping the first element of the list
 
 (define  (sum-up-numbers-simple L)
   (cond
@@ -30,18 +32,18 @@
   
 
 ;Question 3
-;Write a function (sum-up-numbers-general L). L is a list, which may contain
-;as elements numbers and non-numbers. The result of the function is the sum of all the numbers
-;(including those in nested lists) in L. If there are no such numbers, the result is zero. For
-;example, the result of (sum-up-numbers-general ‘(a b 1 (2 c (3)) d))
-;should be 6.
+;Logic
+;The idea here is to check if the first element of the list in an integer or a list itself.
+;1) If null, return 0
+;2) If list, perform an addition by calling the function with the first element as argument and recursively call the function with first elemetn poped
+;3) If a number, add the first number and call the function with first element popped.
+;4) Else, call the function recursively with the first element popped.
 
 (define  (sum-up-numbers-general L)
   (cond
-    ((NULL? L) 0)
-    ((list? (car L)) (+(sum-up-numbers-general (cdr L))  (sum-up-numbers-general  (car L))))
-    ((integer? (car L)) (+ (car L) (sum-up-numbers-general (cdr L)))) 
-    (else (sum-up-numbers-general (cdr L)))))
+    ((NULL? L) 0) ;check if list is NULL.
+    ((list? (car L)) (+(sum-up-numbers-general (cdr L))  (sum-up-numbers-general  (car L)))) ;check for list.
+    ((integer? (car L)) (+ (car L) (sum-up-numbers-general (cdr L)))) ;check for number
+    (else (sum-up-numbers-general (cdr L))))) ;else call the function again
 
-(sum-up-numbers-general '(a 1  9 900 b (200 200) c 300 d) )
-(sum-up-numbers-general '(a 100 b 200 c (300 100) d ) )
+(sum-up-numbers-general '(a (100 200) 300 (400 500)))
